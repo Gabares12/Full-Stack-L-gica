c1a0bcb27f03f0c61ab8b4523398c79be5a9ac50
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-07-07 11:38:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+lert("Bem vindo ao jogo secreto"); 
+let numeroSecreto = 29
+let chute = prompt("escolha o numero secreto de 1 a 40");
+if (chute == numeroSecreto) {
+    console.log('isso ai! Você descobriu o numero secreto (29)');
+}
+
+
