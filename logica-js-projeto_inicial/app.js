48f3e6f0cb271dc51a2f520c3cccc0fc2aa013cf
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-07-09 11:55:54 -0300

    Add files via upload

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,28 @@
+/*
+01 pt1
+lert("Bem vindo ao jogo secreto"); 
+let numeroSecreto = 29
+let chute = prompt("escolha o numero secreto de 1 a 40");
+if (chute == numeroSecreto) {
+    console.log('isso ai! Você descobriu o numero secreto (29)');
+}
+*/
+
+/*01 pt2
+let Dia = prompt ("que dia é hoje?");
+if (Dia =='sabado') {
+    alert ('bom fim de semana!');
+} else if (Dia =='domingo') {
+    alert('bom fim de semana');
+} else {alert
+('bom fim de semana!');
+}
+*/
+/*02 pt2
+let numero = prompt ("numero positivo ou negativo")
+if (numero <'0') {
+    alert ("este numero é negativo")
+}
+else { (numero >'1')
+alert ("este numero é positivo")
+}*/
