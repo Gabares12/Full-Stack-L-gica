bf7efcf24ba4b3496bdafd5b6aa31d2e9ae50278
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-07-11 11:39:21 -0300

    Add files via upload

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,28 +1,90 @@
-/*
-01 pt1
-lert("Bem vindo ao jogo secreto"); 
-let numeroSecreto = 29
-let chute = prompt("escolha o numero secreto de 1 a 40");
+//desafio parte 1
+/*alert ('bem vindo ao jogo do numero secreto');
+let numeroSecreto = 12;
+console.log(numeroSecreto)
+let chute = prompt('tente adivinhar um numero entre 1 e 50');
+
 if (chute == numeroSecreto) {
-    console.log('isso ai! Você descobriu o numero secreto (29)');
-}
+    alert(`isso ai! você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    alert ('voce errou')
+} 
+
+
+
+
+/*
+// desafios parte 1
+alert ('Boas Vindas ao nosso site!');
+
+let nome = "Lua"; 
+
+let idade = "25";
+
+let numeroDeVendas = 50;
+
+let saldoDisponevel = 1000;
+
+alert ('Erro! preencha todos os campos');
+
+let mensagemDeErro = 'Erro! preencha todos os campos';
+
+alert ('mensagemDeErro');
 */
 
-/*01 pt2
-let Dia = prompt ("que dia é hoje?");
-if (Dia =='sabado') {
-    alert ('bom fim de semana!');
-} else if (Dia =='domingo') {
-    alert('bom fim de semana');
-} else {alert
-('bom fim de semana!');
-}
-*/
-/*02 pt2
+
+
+// desafios parte 2
+ 
+let dia = prompt ('qual é o dia da semana?');
+if (dia == 'sabado' || dia == "domingo" ){ 
+    alert ('bom fim de semana') ;
+} else {
+    alert ('boa semana');
+} 
+
+
+
+// desafios parte 2
+/*
 let numero = prompt ("numero positivo ou negativo")
 if (numero <'0') {
     alert ("este numero é negativo")
 }
 else { (numero >'1')
 alert ("este numero é positivo")
-}*/
+}
+*/
+
+
+
+// desafios parte 2
+
+/*
+let saldoConta = 1620;
+alert(`seu saldo da conta é ${saldoConta}`);
+*/
+
+
+
+
+
+
+
+
+// desafios parte 2
+
+/*
+let seuNome = prompt ('Digite seu nome');
+alert (`Bem vindo ao sistema ${seuNome}`);
+
+*/
+
+/*
+let pontucao = prompt('escreva sua pontuacao');
+if ( pontucao >= 110 ) {
+    alert ('voce ganhou'); 
+    } else  {
+        alert ('vc perdeu');
+    };
+  */  
