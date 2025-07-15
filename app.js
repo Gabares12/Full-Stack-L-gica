eda0c3770de9989e0c3b4a77b34c1232d40bef1d
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-07-15 01:14:16 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -79,3 +79,135 @@ let seuNome = prompt ('Digite seu nome');
 alert (`Bem vindo ao sistema ${seuNome}`);
 
 */
+
+
+/*
+let contador = 1;
+while (contador <= 10) {
+    alert(contador);
+    contador++;
+}
+
+let contador2 = 10;
+while (contador2 >= 0) {
+    alert(contador2);
+    contador2--;
+}
+
+
+let contador = 10;
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
+*/
+
+/*
+let numero = prompt("digite o numero para a contagem")
+while (numero >=0) {
+    console.log(numero);
+    numero--;
+}
+
+
+alert ('bem vindo ao jogo do numero secreto');
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+while (chute != numeroSecreto) {
+  chute = (prompt ('tente adivinhar um numero entre 1 a 50'));
+
+if (numeroSecreto == chute) { 
+    break;
+
+} else {
+    if (chute > numeroSecreto ) {
+        alert (`O numero secreto é MENOR que ${chute}`); 
+    } else {
+        alert (`O numero secreto é MAIOR que ${chute}`);
+    }
+     tentativas++;    
+
+}
+
+ }
+let palavraTentativa = tentativas > 1 ? 'tentativa' : 'tentativa' ;
+alert(`isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`); 
+ 
+ //if (tentativas > 1 ) {
+   //  alert(`isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`); 
+   // } else { 
+    //     alert(`isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+    //} 
+    
+*/
+/*
+//1
+console.log('boas-vindas')
+//2
+let nome = ('Gabriel Tavares');
+console.log (`oi ${nome}!`);
+//3
+let nome = ('Gabriel Tavares');
+alert (`oi ${nome}!`);
+//4
+let linguagemGOSTA = prompt ("Qual a linguagem de programação que você mais gosta?")
+console.log (linguagemGOSTA)
+//5
+let Valor1 = 34;
+let Valor2 = 21;
+let resultado = Valor1 + Valor2;
+console.log(`a soma do ${Valor1} + ${Valor2} é ${resultado}`)
+//6
+let Valor1 = 130
+let Valor2 = 230
+let resultado = Valor1 - Valor2;
+console.log (`a subtração de ${Valor1} - ${Valor2} é ${resultado}`)
+
+//7
+let suaIdade = prompt ('digite sua idade');
+if (suaIdade > 17) {
+    console.log ('voce é maior de idade');
+} else {
+    console.log('voce é menor de idade');
+} 
+
+// 8 
+let numero = prompt( 'digite um valor')
+if (numero <'0') {
+    alert ('é negativo') 
+} 
+else { (numero >'1')
+    alert ('é postivo')
+
+}
+// 9
+let numeros = 1;
+while (numeros <=10){
+    console.log(numeros);
+    numeros++;
+}
+
+//10
+let nota = 7;
+if (nota <'9'){
+    console.log ('voce está aprovado');
+    } else {
+          console.log ('voce está reprovado');
+        }
+
+   *
+//11
+ let aleatorio = Math.random();
+ console.log(aleatorio);  
+
+
+//12
+let aleatorio =  parseInt(Math.random() * 10) + 1;
+ console.log(aleatorio); 
+  
+13
+ let aleatorio =  parseInt(Math.random() * 1000) + 1;
+ console.log(aleatorio);
+ */
---
+++ app2.js
@@ -0,0 +1,25 @@
+
+/*
+alert ('bem vindo ao jogo do numero secreto');
+let numeroSecreto = 23;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+while (chute != numeroSecreto) {
+  chute = Number (prompt ('tente adivinhar um numero entre 1 a 50'));
+
+if (numeroSecreto == chute) { 
+    alert(`isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    if (chute > numeroSecreto ) {
+        alert (`O numero secreto é MENOR que ${chute}`); 
+    } else {
+        alert (`O numero secreto é MAIOR que ${chute}`);
+    }
+     tentativas++;    
+
+}
+
+ }
+ 
+*/
